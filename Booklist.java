bfde162b91c57d271399386e1c014647f1a2c752
LN-1106 <workingnly1234@gmail.com>
2025-10-16 00:56:36 +0700

    Update Booklist.java

--- Booklist.java
+++ Booklist.java
@@ -1,37 +1,51 @@
 package Java.project.team;
 import java.util.*;
+
 public class Booklist {
-    private Book[] list;
-    private int n; // số lượng sách
-    //Khởi tạo
-    public Booklist()
+    private ArrayList<Book> list;  // danh sach
+    private int n;                 // số lượng sách
+    private static String List_BOOK = "books.txt"; // statuc cho ten file
+    // Khởi tạo
+    public Booklist() {
+        list = new ArrayList<>();
+        n = 0;
+    }
+    //đọc file
+     public void readFile() 
     {
-        list = new Book[100];
-        n = 0; 
-    };
-    //xuất danh sách
-    public void OutputList(){
+        readFile(List_BOOK);
+    }
+
+    public void readFile(String filename) 
+    {
+        
+    }
+
+    // Xuất danh sách
+    public void OutputList() {
         System.out.println("Các sách hiện tại là:");
-        for(int i = 0; i < n;i++){
-            System.out.print(list[i].toString()); 
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i).toString());
         }
     }
-    //Thêm sách
-    public void AddBook(){
+
+    // Thêm sách
+    public void AddBook() {
 
     }
-    //Xóa sách theo ID
-    public void DeleteBookByID(){
+
+    // Xóa sách theo ID
+    public void DeleteBookByID() {
 
     }
-    //Sửa sách theo ID
-    public void EditBookByID(){
+
+    // Sửa sách theo ID
+    public void EditBookByID() {
 
     }
-    //Tìm sách theo ID
-    public void FindBookByID(){
+
+    // Tìm sách theo ID
+    public void FindBookByID() {
 
     }
 }
-
-
