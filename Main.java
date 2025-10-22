c74dd7717004450bdb638d3f2976d1134aae2d94
synguyen0912327984 <anhsynguyen37@gmail.com>
2025-10-22 08:26:28 +0700

    sua file

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        EmployeeList list = new EmployeeList();
+        EmployeeList listemp = new EmployeeList();
+        CustomerList listCus = new CustomerList();
+        Booklist listb = new Booklist();
+        listb.readFile();
+        ListInvoice listin = new ListInvoice();
+        ListInvoiceDetails listdet = new ListInvoiceDetails();
+        listin.readFile();
+        listdet.readFile();
+        
         
         
     }
